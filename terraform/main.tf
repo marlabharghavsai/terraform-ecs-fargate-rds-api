c179dd1d29fffcfefe1f6cd15e28cc1de4c63a41
marlabharghavsai <marlabharghavsai@gmail.com>
2026-02-15 09:40:44 +0000

    Phase 5 Step 2: Security groups and ECR repository added

--- terraform/main.tf
+++ terraform/main.tf
@@ -31,3 +31,67 @@ resource "aws_subnet" "private_2" {
   cidr_block        = "10.0.4.0/24"
   availability_zone = "us-east-1b"
 }
+
+# ALB Security Group
+resource "aws_security_group" "alb_sg" {
+  name   = "task12-alb-sg"
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# ECS Security Group
+resource "aws_security_group" "ecs_sg" {
+  name   = "task12-ecs-sg"
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port       = 8000
+    to_port         = 8000
+    protocol        = "tcp"
+    security_groups = [aws_security_group.alb_sg.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# RDS Security Group
+resource "aws_security_group" "rds_sg" {
+  name   = "task12-rds-sg"
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port       = 5432
+    to_port         = 5432
+    protocol        = "tcp"
+    security_groups = [aws_security_group.ecs_sg.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_ecr_repository" "repo" {
+  name = "task12-api"
+}
