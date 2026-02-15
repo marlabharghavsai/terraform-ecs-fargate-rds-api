2c77e0667b610ff6d67dc0a3e75ed2fda90f47ac
marlabharghavsai <marlabharghavsai@gmail.com>
2026-02-15 10:28:10 +0000

    Phase 5 Step 5: ALB, target group and listener created

--- terraform/main.tf
+++ terraform/main.tf
@@ -160,3 +160,41 @@ resource "aws_db_instance" "postgres" {
 }
 
 
+# Application Load Balancer
+resource "aws_lb" "alb" {
+  name               = "task12-alb"
+  load_balancer_type = "application"
+  subnets = [
+    aws_subnet.public_1.id,
+    aws_subnet.public_2.id
+  ]
+
+  security_groups = [aws_security_group.alb_sg.id]
+}
+
+# Target Group
+resource "aws_lb_target_group" "tg" {
+  name        = "task12-tg"
+  port        = 8000
+  protocol    = "HTTP"
+  target_type = "ip"
+  vpc_id      = aws_vpc.main.id
+
+  health_check {
+    path = "/health"
+  }
+}
+
+# Listener (Port 80)
+resource "aws_lb_listener" "listener" {
+  load_balancer_arn = aws_lb.alb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.tg.arn
+  }
+}
+
+
